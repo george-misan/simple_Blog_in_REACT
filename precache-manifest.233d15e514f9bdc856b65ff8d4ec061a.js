3344ff19279d9f1789e04061c775eb706b8d1e74
iamgeorge <george.eyoyibo@gmail.com>
2019-11-27 15:16:46 +0200

    Updates

---
+++ precache-manifest.233d15e514f9bdc856b65ff8d4ec061a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5f67d848eba7e388e6d9f2696970cc",
+    "url": "/simple_Blog_in_REACT/index.html"
+  },
+  {
+    "revision": "41b5224a2469c79445c9",
+    "url": "/simple_Blog_in_REACT/static/css/main.171807e0.chunk.css"
+  },
+  {
+    "revision": "56e890021d6072fa825c",
+    "url": "/simple_Blog_in_REACT/static/js/2.a9926313.chunk.js"
+  },
+  {
+    "revision": "41b5224a2469c79445c9",
+    "url": "/simple_Blog_in_REACT/static/js/main.0f8b13c4.chunk.js"
+  },
+  {
+    "revision": "4cbaceb872f7441e22a1",
+    "url": "/simple_Blog_in_REACT/static/js/runtime-main.b14ca65e.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/simple_Blog_in_REACT/static/media/pokeball.f10bf078.png"
+  }
+]);
